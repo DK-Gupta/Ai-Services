a5715203a52015004bbeed6477ddbb0aadce1357
Dhirendra kumar gupta <dkg700731@gmail.com>
2025-04-27 17:33:10 +0530

    add gunicorn to run it

--- ai_services/main.py
+++ ai_services/main.py
@@ -37,4 +37,4 @@ def analyze_traffic():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
